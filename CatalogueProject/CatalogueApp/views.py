e5dfb22e667e66d34de5eed3dc915fd8402e69e1
Venkatesh Kumar <venkatesh.vk677@gmail.com>
2019-12-24 13:48:00 +0530

    Completed with views.py using viewset

--- CatalogueProject/CatalogueApp/views.py
+++ CatalogueProject/CatalogueApp/views.py
@@ -1,3 +1,75 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import BrandSerializer, CategorySerializer, ProductSerializer, ProductCreateSerializer
+from .models import Brand, Category, Product
 # Create your views here.
+
+
+class BrandViewSet(viewsets.ViewSet):
+    """To get all the list of brands"""
+    def list(self, request):
+        queryset = Brand.objects.all()
+        serializer = BrandSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    """To retrieve a particular data"""
+    def retrieve(self, request, pk=None):
+        queryset= Brand.objects.all()
+        brand = get_object_or_404(queryset, pk=pk)
+        serializer = BrandSerializer(brand)
+        return Response(serializer.data)
+
+    """To create a new brand"""
+    def create(self, request):
+        serializer = BrandSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class CategoryViewSet(viewsets.ViewSet):
+    """To get all the list of categories"""
+    def list(self, request):
+        queryset = Category.objects.all()
+        serializer = CategorySerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    """To retrieve a particular data of category"""
+    def retrieve(self, request, pk=None):
+        queryset= Category.objects.all()
+        category = get_object_or_404(queryset, pk=pk)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    """To create a new Category"""
+    def create(self, request):
+        serializer = CategorySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class ProductViewSet(viewsets.ViewSet):
+    """To get all the list of products"""
+    def list(self, request):
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    """To retrieve a particular data of product"""
+    def retrieve(self, request, pk=None):
+        queryset= Product.objects.all()
+        product = get_object_or_404(queryset, pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    """To create a new Product"""
+    def create(self, request):
+        serializer = ProductCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
